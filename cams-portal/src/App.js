602c9a7c05d4bff4310eb217de86ea570fb85692
Codefred <alfredemmanuelinyang@gmail.com>
2020-08-15 13:19:28 +0100

    add popup for legend

--- cams-portal/src/App.js
+++ cams-portal/src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import NavigationBar from './components/NavigationBar'
+import NavigationBar from './components/NavigationBar';
+import Popup from './components/Popup';
 
-function App() {
-  return (
-    <div className="App">
-      <NavigationBar />
-    </div>
-  );
+class App extends Component {
+  state = {
+    showPopup: false
+  }
+
+  togglePopup = () => {
+    console.log("works", this.state.showPopup);
+    this.setState({ showPopup: !this.state.showPopup })
+  }
+  
+  render () {
+    return (
+      <div className="App">
+        <NavigationBar />
+
+        <button className="switch" onClick={this.togglePopup}>Click!</button>
+
+        {
+          this.state.showPopup ?
+          <Popup
+            text='Legend: Inclusive of web portal directives'
+            closePopup={this.togglePopup}
+          />
+          : null
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
-
 
