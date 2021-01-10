29ce046dea173b33e82742822584c9a17d4e9838
jugnugs <jnguyen3@ufl.edu>
2021-01-10 18:28:37 -0500

    adjust styling & add slider event handlers

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,21 +1,39 @@
+import { doc } from 'prettier';
 import React, { useState } from 'react';
 import './timeline.css';
 
 const Slider = (props) => {
-    return (
-      <>
-        <div class="slider">
-          <div class="thumb"></div>
-        </div>
-      </>
-    );
-  }
+  const sliderRef = React.useRef();
+  const thumbRef = React.useRef();
+
+  const handleMouseMove = (e) => {
+    console.log("mouse moved");
+  };
+
+  const handleMouseUp = () => {
+    document.removeEventListener('mouseup', handleMouseUp);
+    document.removeEventListener('mousemove', handleMouseMove);
+  };
+
+  const handleMouseDown = (e) => {
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+  };
+
+  return (
+    <>
+      <div class="slider" ref={sliderRef}>
+        <div class="thumb" ref={thumbRef} onMouseDown={handleMouseDown}></div>
+      </div>
+    </>
+  );
+};
 
 const Timeline = (props) => {
   return (
     <div class="timeline-container">
       <div class="timeline">
-          <Slider />
+        <Slider />
       </div>
     </div>
   );
